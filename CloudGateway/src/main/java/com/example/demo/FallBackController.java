6f5600df5374838a204cfe85475691cb56a8efc1
MohanRaj <mraj86698@gmail.com>
2022-11-25 10:48:33 +0530

    Added to Api Gateways and FallBack Services

--- CloudGateway/src/main/java/com/example/demo/FallBackController.java
+++ CloudGateway/src/main/java/com/example/demo/FallBackController.java
@@ -7,13 +7,13 @@ import reactor.core.publisher.Mono;
 
 @RestController
 public class FallBackController {
-	@RequestMapping("/orderFallBack")
-    public Mono<String> orderServiceFallBack() {
-        return Mono.just("Order Service is taking too long to respond or is down. Please try again later");
-    }
-    @RequestMapping("/paymentFallback")
-    public Mono<String> paymentServiceFallBack() {
-        return Mono.just("Payment Service is taking too long to respond or is down. Please try again later");
-    }
+	 @RequestMapping("/orderFallBack")
+	    public Mono<String> orderServiceFallBack() {
+	        return Mono.just("Order Service is taking too long to respond or is down. Please try again later");
+	    }
+	    @RequestMapping("/paymentFallback")
+	    public Mono<String> paymentServiceFallBack() {
+	        return Mono.just("Payment Service is taking too long to respond or is down. Please try again later");
+	    }
 
 }
